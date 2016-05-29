3e80a0bf348c35ade517b852f4d5d6bf94e92688
Stephen Biston <sjb9774@gmail.com>
2016-05-28 21:54:30 -0500

    one more, promise

--- reload_db.py
+++ reload_db.py
@@ -2,5 +2,4 @@ from mtg_api import db
 from mtg_api.models.magic import *
 from mtg_api.models.users import *
 from mtg_api.models.sessions import *
-from mtg_api.models.decks import *
 db.drop_db()
